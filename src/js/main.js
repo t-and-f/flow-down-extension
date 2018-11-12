decf5ff2f56f6aeb6ea99a0fe7cf4570de5dbce5
Mauro Colella <mauro@thoughtandfunction.com>
2018-11-12 18:16:07 +0800

    :sparkles: Setup templates

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,29 @@
+import {
+    template
+} from 'lodash';
+import notfoundTpl from '../views/notfound.tpl.html';
+
+function empty(node) {
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+}
+
+function present(node, snapshot) {
+    console.log(snapshot);
+    node.innerHTML = snapshot;
+}
+
 chrome.devtools.panels.create("FlowDown",
     "",
     "panel.html",
     function(panel) {
-        panel.onShown.addListener(function(devwin) {
-            const $ = devwin.document.querySelectorAll.bind(devwin.document);
-            $display = $('.display')[0];
-            $display.innerText = 'toast';
+        panel.onShown.addListener(function(devpane) {
+            const $ = devpane.document.querySelectorAll.bind(devpane.document);
+            const $display = $('.display')[0];
+            const notfoundView = {
+                render: template(notfoundTpl),
+            };
 
             chrome.devtools.inspectedWindow.eval(
                 "window.__flowDownStores__ !== undefined ? window.__flowDownStores__.get(0).getState() : null",
@@ -14,8 +32,12 @@ chrome.devtools.panels.create("FlowDown",
                         console.log({
                             store: result
                         });
+                        empty($display);
+                        present($display, notfoundView.render());
                     } else {
                         console.debug(exceptionInfo);
+                        empty($display);
+                        present($display, notfoundView.render());
                     }
                 }
             );
--- webpack.config.js
+++ webpack.config.js
@@ -10,15 +10,23 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'html-loader'
                 }
             }
-        }]
+        ]
     },
     stats: {
         colors: true
