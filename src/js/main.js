77ad911a0e4d31b5c44ac2dcbfb080d79c1a9c36
Mauro Colella <mauro@thoughtandfunction.com>
2018-11-13 14:20:01 +0800

    :sparkles: Add basic diff display

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,17 @@
+import diff from 'deep-diff';
+
+// Libs
 import UI from '../lib/ui';
 import viewsFactory from '../lib/viewsFactory';
 import {
     highlightSyntax
 } from '../lib/utils';
 
+// Templates
 import notfoundTpl from '../views/notfound.tpl.html';
 import stateTreeTpl from '../views/statetree.tpl.html';
 
-
+// Create a devtools panel
 chrome.devtools.panels.create("FlowDown",
     "",
     "panel.html",
@@ -16,6 +20,7 @@ chrome.devtools.panels.create("FlowDown",
             panel,
         });
 
+        // Initialize
         panel.onShown.addListener(function(devpane) {
             const $ = devpane.document.querySelectorAll.bind(devpane.document);
             const ui = new UI($('.display')[0]);
@@ -24,18 +29,24 @@ chrome.devtools.panels.create("FlowDown",
             const stateTreeView = viewsFactory.getView(stateTreeTpl);
 
             let stateCache = null;
+            let delta = null;
 
+            // Poll for store
             function poll() {
                 chrome.devtools.inspectedWindow.eval(
                     "window.__flowDownStores__ !== undefined ? window.__flowDownStores__.get(0).getState() : null",
                     function(result, exceptionInfo) {
                         if (result) {
                             if (JSON.stringify(result) !== JSON.stringify(stateCache)) {
+                                delta = diff(result, stateCache);
                                 stateCache = Object.assign({}, result);
+
                                 const str = JSON.stringify(result, null, 4);
+                                const status = JSON.stringify(delta, null, 4);
+
                                 ui.present(stateTreeView.render({
                                     payload: highlightSyntax(str),
-                                    status: 'TBD',
+                                    status,
                                 }));
                             }
                         } else {
@@ -49,6 +60,7 @@ chrome.devtools.panels.create("FlowDown",
                 }, 50);
             }
 
+            // Start
             poll();
         });
     }
